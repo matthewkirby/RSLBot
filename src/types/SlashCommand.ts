5fa40b12500e683836b430e76a3b04fd96162632
Xopar <xoparr@gmail.com>
2023-08-09 03:29:42 -0500

    Tighten type on permission of slashcommands

--- src/types/SlashCommand.ts
+++ src/types/SlashCommand.ts
@@ -3,6 +3,6 @@ import { ChatInputCommandInteraction } from 'discord.js';
 export interface SlashCommand {
     name: string;
     description: string;
-    requiredPermission: number;
+    requiredPermission: 1 | 2 | 3 | 4;
     execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
 }
