e971016e5c39cdfca1f98e10a8351097fbe54ad9
Xopar <xoparr@gmail.com>
2023-08-10 16:56:49 -0500

    Fix bot dming every person that posts in any channel it can read

--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,9 +1,10 @@
-import { Message } from 'discord.js';
+import { ChannelType, Message } from 'discord.js';
 import { FatClient } from 'types/FatClient';
 
 export default (client: FatClient): void => {
   client.on('messageCreate', async (message: Message) => {
     if (message.author.bot) { return; }
+    if (message.channel.type !== ChannelType.DM) { return; }
 
     const client = message.client as FatClient;
     const row = client.actionRows.get('messageCreate');
